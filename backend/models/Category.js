36af6d634df488472222077eb79997effd894f35
Naman Soni <namantsoni@gmail.com>
2024-07-16 12:09:14 +0530

    Data Modeling

---
+++ backend/models/Category.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+        default: "Uncategorized",
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ["income", "expense"],
+    },
+}, {
+    timestamps: true,
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+export default Category;
