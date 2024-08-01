c461db75daa133d850367b80c19e18c4f03228cb
Naman Soni <namantsoni@gmail.com>
2024-08-01 22:50:31 +0530

    Configuring redux and react query

---
+++ frontend/src/redux/slice/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//Initial State
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+    },
+    //Reducers
+    reducers: {
+        loginAction: (state, action) => {
+            state.user = action.payload;
+        },
+        logoutAction: (state, action) => {
+            state.user = null
+        }
+    },
+});
+
+//Generate Actions
+export const { loginAction, logoutAction } = authSlice.actions;
+
+//Generate Reducers
+const authReducer = authSlice.reducer;
+export default authReducer;
